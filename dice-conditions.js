4d6f912a13f6f22f81bc68391a7950b1820a87ef
superseva <superseva@gmail.com>
2020-11-11 12:02:49 +0100

    Added Script

---
+++ dice-conditions.js
@@ -0,0 +1,5 @@
+Hooks.on("init", () => {
+    CONFIG.statusEffects.unshift("assets/genesys/die/b.png");
+    CONFIG.statusEffects.unshift("assets/genesys/die/s.png");
+    CONFIG.statusEffects.unshift("assets/genesys/die/c.png");
+});
