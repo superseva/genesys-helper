28e6fabf44591a6fe4ddd544b67385ceaf662c7b
superseva <superseva@gmail.com>
2020-11-11 12:08:53 +0100

    improved script

--- dice-conditions.js
+++ dice-conditions.js
@@ -2,4 +2,7 @@ Hooks.on("init", () => {
     CONFIG.statusEffects.unshift("assets/genesys/die/b.png");
     CONFIG.statusEffects.unshift("assets/genesys/die/s.png");
     CONFIG.statusEffects.unshift("assets/genesys/die/c.png");
+
+    /*in case you only want your icons uncomment the line bellow and set your image paths:*/
+    //CONFIG.statusEffects = ["assets/genesys/die/b.png", "assets/genesys/die/s.png", "assets/genesys/die/c.png"]
 });
