8d573bc6d9b2e26e7c88b2a289f2dd34c92dc81a
superseva <superseva@gmail.com>
2020-11-11 12:42:53 +0100

    updated

--- dice-conditions.js
+++ dice-conditions.js
@@ -1,9 +1,9 @@
 Hooks.on("init", () => {
-    //paths for the images start at your data folder. I have the 'assets' folder in Foundry data folder - next to my 'worlds' and 'systems' folders
-    CONFIG.statusEffects.unshift("assets/genesys/die/b.png");
-    CONFIG.statusEffects.unshift("assets/genesys/die/s.png");
-    CONFIG.statusEffects.unshift("assets/genesys/die/c.png");
+    //Add them to the other icons
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/c.png");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/s.png");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/b.png");
 
     /*in case you only want your icons and no other icons you can uncomment the line bellow and set your image paths:*/
-    //CONFIG.statusEffects = ["assets/genesys/die/b.png", "assets/genesys/die/s.png", "assets/genesys/die/c.png"]
+    //CONFIG.statusEffects = ["modules/genesys-helper/die/b.png", "modules/genesys-helper/die/s.png", "modules/genesys-helper/die/c.png"]
 });
