1ebc96cc52346260010099c9da85047d244f7fb3
superseva <superseva@gmail.com>
2023-06-15 12:26:29 -0500

    added pics

--- dice-conditions.js
+++ dice-conditions.js
@@ -1,8 +1,11 @@
-Hooks.on("init", () => {
+Hooks.on("setup", () => {
     //Add them to the other icons
-    CONFIG.statusEffects.unshift("modules/genesys-helper/die/c.png");
-    CONFIG.statusEffects.unshift("modules/genesys-helper/die/s.png");
-    CONFIG.statusEffects.unshift("modules/genesys-helper/die/b.png");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-blue.png");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-black.png");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-green.png");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-purple.png");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-yellow.png");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-red.png");
 
     /*in case you only want your icons and no other icons you can uncomment the line bellow and set your image paths:*/
     //CONFIG.statusEffects = ["modules/genesys-helper/die/b.png", "modules/genesys-helper/die/s.png", "modules/genesys-helper/die/c.png"]
