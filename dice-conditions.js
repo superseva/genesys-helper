840a1ec459a6ff649e4ccac7ade32adc9d8810b1
superseva <superseva@gmail.com>
2023-06-15 12:28:34 -0500

    webp

--- dice-conditions.js
+++ dice-conditions.js
@@ -1,12 +1,12 @@
 Hooks.on("setup", () => {
     //Add them to the other icons
-    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-blue.png");
-    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-black.png");
-    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-green.png");
-    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-purple.png");
-    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-yellow.png");
-    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-red.png");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-blue.webp");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-black.webp");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-green.webp");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-purple.webp");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-yellow.webp");
+    CONFIG.statusEffects.unshift("modules/genesys-helper/die/die-red.webp");
 
     /*in case you only want your icons and no other icons you can uncomment the line bellow and set your image paths:*/
-    //CONFIG.statusEffects = ["modules/genesys-helper/die/b.png", "modules/genesys-helper/die/s.png", "modules/genesys-helper/die/c.png"]
+    //CONFIG.statusEffects = ["modules/genesys-helper/die/b.webp", "modules/genesys-helper/die/s.webp", "modules/genesys-helper/die/c.webp"]
 });
